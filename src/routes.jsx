120aa725c01eef67adfe002e2bd077e5f62131c0
victorngigi <victor.ngigi@outlook.com>
2025-06-25 22:33:18 +0300

    Revert "Revert 8 marvin"

---
+++ src/routes.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import LandingPage from './pages/LandingPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+import DashboardPage from './pages/DashboardPage.jsx';
+import WorkoutsPage from './pages/WorkoutsPage.jsx';
+import WorkoutDetailPage from './pages/WorkoutDetailPage.jsx';
+import CreateWorkoutPage from './pages/CreateWorkoutPage.jsx';
+import EditWorkoutPage from './pages/EditWorkoutPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import ProgressPage from './pages/ProgressPage.jsx';
+
+
+
+export const myRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <LandingPage /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'register', element: <RegisterPage /> },
+      {element: <ProtectedRoute />,
+        children: [
+          { path: 'dashboard', element: <DashboardPage /> },
+          { path: 'workouts', element: <WorkoutsPage /> },
+          { path: 'workouts/new', element: <CreateWorkoutPage /> },
+          { path: 'workouts/edit/:id', element: <EditWorkoutPage /> },
+          { path: 'workouts/:id', element: <WorkoutDetailPage /> },
+          { path: 'profile', element: <ProfilePage /> },
+          { path: 'progress', element: <ProgressPage /> },
+          { path: '*', element: <NotFoundPage /> },
+        ]
+      }
+      
+    ],
+  },
+]);
