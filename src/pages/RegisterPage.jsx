383a67ea2a126873cd2ce4de82a4c8b2f65471a7
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-06-25 17:37:26 +0300

    fixed import from login to register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../api/api';
-import LoginPage from './LoginPage';
+import login from './LoginPage';
 
 export default function RegisterPage() {
   const navigate = useNavigate();
